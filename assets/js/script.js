11fee835c5ee9ed4703014b0f3944b73a8fa8b58
Cory Piette <coryjpiette@gmail.com>
2021-06-14 18:16:34 -0400

    adding code to hide and unhide sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,24 @@ let scoreCount = 0;
 
 let timerReset;
 
-let answers = document.querySelectorAll('#quizTaker button')
+let answers = document.querySelectorAll('#answerOptions button')
 
 let scoreArray = {};
 
 (localStorage.getItem('scoreArray')) ? scoreArray = JSON.parse(localStorage.getItem('scoreArray')):scoreArray=[];
+
+let queryElement = (element) => {
+    return document.querySelector(element);
+}
+
+let showSection = (element) => {
+    let sections = document.querySelectorAll("section")
+
+    
+    Array.from(sections).forEach((userInput) => {
+        userInput.classList.remove('hide');
+    
+});
+queryElement(element).classList.remove('hide');
+}
+
