86e9d7fb58c1220e029f52caf237ccd433032eec
Cory Piette <coryjpiette@gmail.com>
2021-06-14 19:12:12 -0400

    resetting to show score

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,11 +23,24 @@ let queryElement = (element) => {
 let showSection = (element) => {
     let sections = document.querySelectorAll("section")
 
-    
+
     Array.from(sections).forEach((userInput) => {
         userInput.classList.remove('hide');
     
 });
 queryElement(element).classList.remove('hide');
 }
+
+let clearRecords = () => {
+    queryElement('#highScores" div').data="";
+    var i=1;
+scoreArray.sort((a,b) = b.score - a.score);
+Array.from(scoreArray).forEach(check =>
+    {
+
+    var scores = document.createElement("div")
+    scores.data =i + "." + check.firstScore+ "-" +check.score;
+    queryElement('#highScores div').appendChild(scores);
+    i=1+1
+    });
 
