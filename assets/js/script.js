dedc22ae17220be69641438e8d4e27221cd4c3f5
Cory Piette <coryjpiette@gmail.com>
2021-06-14 20:17:05 -0400

    cleaning up formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 
 var timerStart = 75;
 
-let timer =75;
+let timer = 75;
 
 let score = 0;
 
@@ -14,7 +14,7 @@ let answers = document.querySelectorAll('#answerOptions button')
 
 let scoreArray = {};
 
-(localStorage.getItem('scoreArray')) ? scoreArray = JSON.parse(localStorage.getItem('scoreArray')):scoreArray=[];
+(localStorage.getItem('scoreArray')) ? scoreArray = JSON.parse(localStorage.getItem('scoreArray')) : scoreArray = [];
 
 let queryElement = (element) => {
     return document.querySelector(element);
@@ -26,33 +26,32 @@ let showSection = (element) => {
 
     Array.from(sections).forEach((userInput) => {
         userInput.classList.remove('hide');
-    
-});
-queryElement(element).classList.remove('hide');
+
+    });
+    queryElement(element).classList.remove('hide');
 }
 
 let clearRecords = () => {
-    queryElement('#highScores" div').data="";
-    var i=1;
-scoreArray.sort((a,b) = b.score - a.score);
-Array.from(scoreArray).forEach(check =>
-    {
+    queryElement('#highScores" div').data = "";
+    var i = 1;
+    scoreArray.sort((a, b) = b.score - a.score);
+    Array.from(scoreArray).forEach(check => {
 
-    var scores = document.createElement("div")
-    scores.data =i + "." + check.firstScore+ "-" +check.score;
-    queryElement('#highScores div').appendChild(scores);
-    i=1+1
+        var scores = document.createElement("div")
+        scores.data = i + "." + check.firstScore + "-" + check.score;
+        queryElement('#highScores div').appendChild(scores);
+        i = 1 + 1
     });
 
-i=0;
-Array.from(answers)forEach(answer => {
-    answer.classList.remove('disable')
-})
+    i = 0;
+    Array.from(answers)forEach(answer => {
+        answer.classList.remove('disable')
+    })
 
 }
 
 let startQuestions = () => {
 
-queryElement('#answerOptionss p').data = questions[quesrtionNumber].answerText;
+    queryElement('#answerOptionss p').data = questions[quesrtionNumber].answerText;
 
-queryElement('#answerOptions button:
+    queryElement('#answerOptions button:
